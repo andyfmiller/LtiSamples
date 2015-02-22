4bbfc396b33792332bdea0add46900a8641d31e4
andyfmiller <aamiller@alumni.princeton.edu>
2015-02-22 10:55:55 -0800

    Renamed ToolConsumerProfileApiController and implemented new OnGetToolConsumerProfile Func<>.

---
+++ Consumer/Controllers/ToolConsumerProfileController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Consumer.Lti;
+using LtiLibrary.AspNet.Profiles;
+using LtiLibrary.Core.Common;
+using LtiLibrary.Core.Lti2;
+using LtiLibrary.Core.Profiles;
+
+namespace Consumer.Controllers
+{
+    /// <summary>
+    /// This is a sample ApiController that returns a minimal ToolConsumerProfile.
+    /// </summary>
+    public class ToolConsumerProfileController : ToolConsumerProfileControllerBase
+    {
+        public ToolConsumerProfileController()
+        {
+            OnGetToolConsumerProfile = context =>
+            {
+                // I use AssemblyInfo to store product and vendor values that may be used
+                // in multiple places in this sample
+                var guid = LtiUtility.GetProduct();
+                var code = guid; // Product and ProductFamily are the same in this sample
+                var vendorName = LtiUtility.GetCompany();
+                var productName = LtiUtility.GetTitle();
+                var productVersion = LtiUtility.GetVersion();
+
+                // Build a minimal ToolConsumerProfile for LTI 1.2
+                var profile = new ToolConsumerProfile
+                {
+                    CapabilityOffered = new[] {LtiConstants.BasicLaunchLtiMessageType},
+                    Guid = guid,
+                    LtiVersion = context.LtiVersion,
+                    ProductInstance = new ProductInstance
+                    {
+                        Guid = guid,
+                        ProductInfo = new ProductInfo
+                        {
+                            ProductFamily = new ProductFamily
+                            {
+                                Code = code,
+                                Vendor = new Vendor
+                                {
+                                    Code = code,
+                                    Timestamp = DateTime.UtcNow,
+                                    VendorName = new LocalizedName
+                                    {
+                                        Key = "product.vendor.name",
+                                        Value = vendorName
+                                    }
+                                }
+                            },
+                            ProductName = new LocalizedName
+                            {
+                                Key = "product.name",
+                                Value = productName
+                            },
+                            ProductVersion = productVersion
+                        }
+                    }
+                };
+
+                // Add Outcomes Management
+                var outcomesUrl = UrlHelper.GenerateUrl("DefaultApi", null, "Outcomes",
+                    new RouteValueDictionary {{"httproute", string.Empty}}, RouteTable.Routes,
+                    HttpContext.Current.Request.RequestContext, false);
+                Uri serviceUri;
+                profile.ServiceOffered = new[]
+                {
+                    new RestService
+                    {
+                        Action = new[] {"POST"},
+                        EndPoint = Uri.TryCreate(Request.RequestUri, outcomesUrl, out serviceUri) ? serviceUri : null,
+                        Format = new[] {LtiConstants.OutcomeMediaType}
+                    }
+                };
+                context.ToolConsumerProfile = profile;
+                return Task.FromResult<object>(null);
+            };
+        }
+    }
+}
